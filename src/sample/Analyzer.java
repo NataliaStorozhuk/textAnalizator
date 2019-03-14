bdb73e6910e4f227eb340a562514823f5a3e511b
NStorozhuk <storozhuk2909@mail.ru>
2019-03-14 14:07:00 +0300

    Добавили и настроили ломбок, poi, запустили тестовый пример записи в эксельку

--- src/sample/Analyzer.java
+++ src/sample/Analyzer.java
@@ -1,6 +1,8 @@
 package sample;
 
 
+import lombok.Setter;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -12,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Setter
 public class Analyzer {
     int n = 3;
 
